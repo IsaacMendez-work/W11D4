e75c02bf971375deb3c1baf7adb1a99ce2e6d491
Eyke Mendez <73685690+IsaacMendez-work@users.noreply.github.com>
2021-04-29 11:38:08 -0400

    adding pseudocode in a verified commit

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Tour of Heroes'; // This title is being called in app.component.html on line 346 as "<span>{{ title }} app is running!</span>", where {{ title }} is available because we're exporting this AppComponent
 }
